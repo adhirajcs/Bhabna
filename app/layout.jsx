7a2d4f97e8bed1d80cee6181acf42cb57e8fa941
Adhiraj Saha <93763060+adhirajcs@users.noreply.github.com>
2024-08-07 11:17:28 +0000

    Added favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,8 @@
 import '@styles/globals.css'
 
+import Nav from '@components/Nav'
+import Provider from '@components/Provider'
+
 export const metadata = {
     title: 'Bhabna',
     description: 'Discover and share Ideas',
@@ -8,12 +11,16 @@ export const metadata = {
 const RootLayout = ({ children }) => {
   return (
     <html lang='en'>
+        <head>
+        <link rel="icon" type="image/svg+xml" href="/assets/images/bhabna_logo.png" />
+        </head>
         <body>
             <div className="main">
                 <div className="gradient" />
             </div>
 
             <main className="app">
+                <Nav />
                 {children}
             </main>
         </body>
