8899121e3bf610e1005122f198d025d83ab7b1ab
Adhiraj Saha <93763060+adhirajcs@users.noreply.github.com>
2024-08-13 10:04:44 +0000

    Added Google Auth Provider

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,32 +1,39 @@
-import '@styles/globals.css'
+import "@styles/globals.css";
 
-import Nav from '@components/Nav'
-import Provider from '@components/Provider'
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
-    title: 'ভাবনা (Bhabna)',
-    description: 'Discover and share Ideas',
-    }
+  title: "ভাবনা (Bhabna)",
+  description: "Discover and share Ideas",
+};
 
 const RootLayout = ({ children }) => {
   return (
-    <html lang='en'>
-        <head>
-        <link rel="icon" type="image/svg+xml" href="/assets/images/bhabna_logo.png" />
-        </head>
-        <body>
-            <div className="main">
-                <div className="gradient" />
-            </div>
+    <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href="/assets/images/bhabna_logo.png"
+        />
+      </head>
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
 
-            <main className="app">
-                <Nav />
-                {children}
-            </main>
-        </body>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
 
+        
+      </body>
     </html>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
