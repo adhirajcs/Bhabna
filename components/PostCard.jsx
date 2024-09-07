51bf66db85364b764f6e8ebf41ffe978794a65fb
Adhiraj Saha <93763060+adhirajcs@users.noreply.github.com>
2024-09-07 15:22:37 +0000

    Added Tooltip for view profile

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -28,8 +28,9 @@ const PostCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
     <>
       <div className="post_card">
         <div className="flex justify-between items-start gap-5">
+          {/* Profile Name with Tooltip */}
           <div
-            className="flex-1 flex justify-start items-center gap-3 cursor-pointer"
+            className="group relative flex-1 flex justify-start items-center gap-3 cursor-pointer"
             onClick={handleCreatorClick}
           >
             <Image
@@ -47,7 +48,16 @@ const PostCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
                 {post.creator.email}
               </p>
             </div>
+            {/* Tooltip for "View Profile" */}
+            <div className="bg-gray-200 p-2 rounded-md hidden group-hover:flex absolute -top-2 -translate-y-full left-[40%] -translate-x-[50%]">
+              <span className="text-gray-500 whitespace-nowrap">
+                View Profile
+              </span>
+              <div className="bg-inherit rotate-45 p-1 absolute bottom-0 translate-y-1/2 left-1/2 -translate-x-1/2"></div>
+            </div>
           </div>
+
+          {/* Copy Button */}
           <div className="copy_btn" onClick={handleCopy}>
             <Image
               src={
@@ -61,11 +71,12 @@ const PostCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
             />
           </div>
         </div>
-
-        {/* whitespace-pre-wrap is used to preserve the line breaks of the actual post_body from the DB  */}
+        {/* Post Body */}
+        {/* Note: whitespace-pre-wrap is used to preserve the line breaks of the actual post_body from the DB  */}
         <p className="my-4 font-satoshi text-sm text-gray-700 whitespace-pre-wrap">
           {post.post_body}
         </p>
+        {/* Post Tag */}
         <p
           className="font-inter text-sm blue_gradient cursor-pointer"
           onClick={() => handleTagClick && handleTagClick(post.tag)}
@@ -73,6 +84,7 @@ const PostCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
           {post.tag}
         </p>
 
+        {/* Edit/Delete Buttons for Own Post */}
         {session?.user.id === post.creator._id && pathName === "/profile" && (
           <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
             <p
